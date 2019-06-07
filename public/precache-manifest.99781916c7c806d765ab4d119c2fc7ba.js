1eb2bdbe93a8b66b2c012c178132d698d6708c83
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-07 11:20:52 +0400

    Seems fine

---
+++ public/precache-manifest.99781916c7c806d765ab4d119c2fc7ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024b923e559d1ed0d9d13ba32851f8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f99ed14664d083da5285",
+    "url": "/static/css/main.7cc714e5.chunk.css"
+  },
+  {
+    "revision": "3a8f8d03e682765561b3",
+    "url": "/static/js/2.6c2ab92a.chunk.js"
+  },
+  {
+    "revision": "f99ed14664d083da5285",
+    "url": "/static/js/main.29185b28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
